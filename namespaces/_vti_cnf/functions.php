0c9ad887c30939c30fd0f03efd67442d71433835
Sara <sara.puljic92@gmail.com>
2015-06-16 14:02:03 +0200

    commit

---
+++ namespaces/_vti_cnf/functions.php
@@ -0,0 +1,15 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|Sara-PC\\Sara
+vti_modifiedby:SR|Sara-PC\\Sara
+vti_timelastmodified:TR|16 Jun 2015 12:01:24 -0000
+vti_timecreated:TR|16 Jun 2015 11:17:04 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TW|16 Jun 2015 11:17:07 -0000
+vti_cacheddtm:TX|16 Jun 2015 12:01:24 -0000
+vti_filesize:IR|11
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|utf-8
